8890c51e7a63976650063da40edce73bc17aeaf5
Teo Mastro <teomastro1999@gmail.com>
2020-12-18 16:25:22 +0200

    TM - added game_initializer-WIP login-WIP fill board

--- index.php
+++ index.php
@@ -9,7 +9,17 @@
 <body>
 
 <h2>Connect 4</h2>
-
+<div id='game_initializer'>
+  <input id='username'>
+  <select id='chooseColor'>
+    <option value='R'>Red</option>
+    <option value='Y'>Yellow</option>
+  </select>
+  <button id='connect4_login'>ΕΙΣΟΔΟΣ ΣΤΟ ΠΑΙΧΝΙΔΙ</button>
+</div>
+<div id='move_div'>
+  
+</div>
 <div id="connect4_board_div">
 
 </body>
--- js/connect4.js
+++ js/connect4.js
@@ -1,6 +1,6 @@
 $(function() {
     draw_empty_board();
-
+    fill_board();
 });
 
 function draw_empty_board(p) {
@@ -15,4 +15,48 @@ for (var i = 1; i <= 6; i++) {
 board += '</table>'
 
 $('#connect4_board_div').html(board);
+}
+
+function fill_board(){
+  $.ajax({url: "connect4.php/board/", success: fill_board_with_data });
+}
+
+function fill_board_with_data(data){
+  for(var i=0; data.length ; i++){
+    var o = data[i];
+    var id = '#square_'+o.x +'_'+o.y;
+    //var c = (o.piece!=null)?o.piece_color+o.piece:'';
+    //$(id).addClass(o.b_color+'#square_').html(in);
+  }
+}
+
+function login_to_game(){
+  if($('#username').val()==''){
+    alert('Πρέπει να επιλέξετε ένα username');
+    return;
+  }
+  var chooseColor = $('#chooseColor').val();
+  draw_empty_board(chooseColor);
+  fill_board();
+
+  $.ajax({
+    url: "connect4.php/players/"+chooseColor,
+    method: 'PUT',
+    dataType: 'json',
+    contentType: 'application/json',
+    data: JSON.stringify({ nickname: $('#username').val(), pawn_color: $('#chooseColor').val() }),
+    success: login_res,
+    error: login_er
+  })
+}
+
+function login_res(data){
+  mes = data[0];
+  $('#game_initializer').hide();
+  //updates
+}
+
+function login_er(data,y,z,c){
+  var x = data.responseJSON;
+  alert(x.console.error());
 }

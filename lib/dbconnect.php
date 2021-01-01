5ac5dc505f5f6ef91fc2ed3b66a1e9c4f558db93
vagmark <vagmark30@gmail.com>
2021-01-01 21:22:24 +0200

    EM - WIP ACCESS FROM USERS.TEITHE

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,14 +1,15 @@
 <?php
 require_once "local_config.php";
-$DB_USER = "root";
-$DB_PASS = "";
 
-$host='localhost'; //vag esy to allazeis auto se localhost sketo.
+$host='localhost';
 $db = 'connect4';
 $user=$DB_USER;
 $pass=$DB_PASS;
-global $mysqli;
-$mysqli = new mysqli($host, $user, $pass, $db);
+if(gethostname()=='users.iee.ihu.gr') {
+	$mysqli = new mysqli($host, $user, $pass, $db,null,'/home/student/it/2017/it174966/mysql/run/mysql.sock');
+} else {
+        $mysqli = new mysqli($host, $user, $pass, $db);
+}
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" .
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
